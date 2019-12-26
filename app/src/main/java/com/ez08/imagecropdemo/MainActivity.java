22aec29599613d1036848332f829297517a171fa
xulimin <592626355@qq.com>
2019-12-26 13:41:33 +0800

    增加了图片裁剪，使用了CircleimageView

--- app/src/main/java/com/ez08/imagecropdemo/MainActivity.java
+++ app/src/main/java/com/ez08/imagecropdemo/MainActivity.java
@@ -36,12 +36,35 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener,EasyPermissions.PermissionCallbacks{
 
-    private ImageView ivTest;
+    private CircleImageView ivTest;
 
     private File cameraSavePath;//拍照照片路径
     private Uri uri;//照片Uri
     //权限
     private String[] permissions = {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private String photoName = System.currentTimeMillis() + ".jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btnGetPicFromCamera = findViewById(R.id.btn_get_pic_from_camera);
+        Button btnGetPicFromPhotoAlbum = findViewById(R.id.btn_get_pic_form_photo_album);
+        Button btnGetPermission = findViewById(R.id.btn_get_Permission);
+        ivTest = findViewById(R.id.iv_test);
+
+        btnGetPicFromCamera.setOnClickListener(this);
+        btnGetPicFromPhotoAlbum.setOnClickListener(this);
+        btnGetPermission.setOnClickListener(this);
+
+        cameraSavePath = new File(Environment.getExternalStorageDirectory().getPath()+"/"+photoName);
+
+    }
+
+
     //激活相机操作
     private void goCamera(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -64,24 +87,6 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         intent.setAction(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, 2);
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        Button btnGetPicFromCamera = findViewById(R.id.btn_get_pic_from_camera);
-        Button btnGetPicFromPhotoAlbum = findViewById(R.id.btn_get_pic_form_photo_album);
-        Button btnGetPermission = findViewById(R.id.btn_get_Permission);
-        ivTest = findViewById(R.id.iv_test);
-
-        btnGetPicFromCamera.setOnClickListener(this);
-        btnGetPicFromPhotoAlbum.setOnClickListener(this);
-        btnGetPermission.setOnClickListener(this);
-
-        cameraSavePath = new File(Environment.getExternalStorageDirectory().getPath()+"/"+System.currentTimeMillis() + ".jpg");
-
     }
 
 
@@ -125,14 +130,29 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
                 photoPath = uri.getEncodedPath();
             }
             Log.d("拍照返回图片路径:", photoPath);
+           //对拍的照进行裁剪
+            photoClip(uri);
             Glide.with(MainActivity.this).load(photoPath).into(ivTest);
         } else if (requestCode == 2 && resultCode == RESULT_OK) {
             photoPath = getPhotoFromPhotoAlbum.getRealPathFromUri(this, data.getData());
+            Log.d("相册返回图片路径:", photoPath);
+            photoClip(data.getData());
             Glide.with(MainActivity.this).load(photoPath).into(ivTest);
+        }else if (requestCode == 3&& resultCode ==RESULT_OK){
+            Bundle bundle = data.getExtras();
+
+            if (bundle != null) {
+                //在这里获得了剪裁后的Bitmap对象，可以用于上传
+                Bitmap image = bundle.getParcelable("data");
+                //设置到ImageView上
+                ivTest.setImageBitmap(image);
+                //也可以进行一些保存、压缩等操作后上传
+                String path = saveImage("头像", image);
+                Log.d("裁剪路径:", path);
+            }
         }
 
         super.onActivityResult(requestCode, resultCode, data);
-
 
     }
 
@@ -151,4 +171,46 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
                 break;
         }
     }
+    //裁剪图片的方法
+    private void photoClip(Uri uri){
+        //调用系统自带的图片裁剪
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        //赋予临时访问权限
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION|Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.setDataAndType(uri,"image/*");
+        // 下面这个crop=true是设置在开启的Intent中设置显示的VIEW可裁剪
+        intent.putExtra("crop","true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        Uri uritempFile = Uri.parse("file://" + "/" + Environment.getExternalStorageDirectory().getPath() + "/" + System.currentTimeMillis() + ".jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uritempFile);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+
+        startActivityForResult(intent, 3);
+    }
+    public String saveImage(String name, Bitmap bmp) {
+        File appDir = new File(Environment.getExternalStorageDirectory().getPath());
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = name + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+            return file.getAbsolutePath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
